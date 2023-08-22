f375bf80674d081341865e6ba69697abe9663b16
DaBr16 <daniel.brezer@gmail.com>
2023-08-22 20:03:31 +0200

    fix button selection and add div displaying who wins

--- game.js
+++ game.js
@@ -6,28 +6,32 @@ function getComputerChoice() {
 }
 
 // bug: returning always the value of the first button clicked!
-function playRound() {
-    let result = "";
+function playRound(id) {
+    const playerChoice = id;
     const compChoice = getComputerChoice();
-    let playerChoice = document.getElementById("btn");
-    let test = playerChoice.value;
-    // playerChoice = playerChoice.toLowerCase();
-    console.log(test);
-    // if(playerChoice==="rock" && compChoice==="scissors") {
-    //     result = "You Win! Rock beats scissors.";
-    // }else if(playerChoice==="scissors" && compChoice==="paper") {
-    //     result = "You Win! Scissors beats paper.";
-    // }else if(playerChoice==="paper" && compChoice==="rock") {
-    //     result = "You Win! Paper beats rock.";
-    // }else if(compChoice==="rock" && playerChoice==="scissors") {
-    //     result = "You lost! Rock beats scissors.";
-    // }else if(compChoice==="scissors" && playerChoice==="paper") {
-    //     result = "You lost! Scissors beats paper.";
-    // }else if(compChoice==="paper" && playerChoice==="rock") {
-    //     result = "You lost! Paper beats rock.";
-    // }else {
-    //     result = "Draw!"
-    // }
-    // return result;
-    return;
+    const para = document.querySelector("p");
+    if(playerChoice==="rock" && compChoice==="scissors") {
+        result = "You Win! Rock beats scissors.";
+        para.textContent = "You Win! Rock beats scissors.";
+    }else if(playerChoice==="scissors" && compChoice==="paper") {
+        result = "You Win! Scissors beats paper.";
+        para.textContent = "You Win! Scissors beats paper.";
+    }else if(playerChoice==="paper" && compChoice==="rock") {
+        result = "You Win! Paper beats rock.";
+        para.textContent = "You Win! Paper beats rock.";
+    }else if(compChoice==="rock" && playerChoice==="scissors") {
+        result = "You lost! Rock beats scissors.";
+        para.textContent = "You lost! Rock beats scissors.";
+    }else if(compChoice==="scissors" && playerChoice==="paper") {
+        result = "You lost! Scissors beats paper.";
+        para.textContent = "You lost! Scissors beats paper.";
+    }else if(compChoice==="paper" && playerChoice==="rock") {
+        result = "You lost! Paper beats rock.";
+        para.textContent = "You lost! Paper beats rock.";
+    }else {
+        result = "Draw!";
+        para.textContent = "Draw!";
+    }
+    console.log(result);
+    return result;
 }
